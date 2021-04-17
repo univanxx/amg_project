4d829fed1f376503de5aa686b9c8c985d03fc82f
univanxx <82705858+univanxx@users.noreply.github.com>
2021-04-17 20:14:43 +0300

    Связал с выгрузкой в файл и доделал явный метод (#2)

--- Euler2.h
+++ Euler2.h
@@ -5,7 +5,7 @@
 #include <string>
 #include "main.h"
 #include "mesh.h"
-
+#include "vector"
 
 extern int count_points1;
 extern int count_internal_points1;
@@ -41,22 +41,23 @@ public:
     void SaveMeshInGMSHFile();
     void SaveSolutionInGMSHFile();
 
-protected:
-
-    // пересчитывает консервативные переменные в примитивные
-    void Decoding();
-
-    // gmsh-файл в который сохраняется решение
-    std::string m_gmsh_file = "C:/Users/wchhi/Downloads/resultsFM_new.msh";
-    // ДЛЯ ЗАДАЧИ: время = 1, шаг = 0.1
-    double m_time_moment = 1;
-    uint32_t m_step = 0.1;
     // консервативные переменные
     double* m_u0, * m_u1, * m_u2, * m_u3, * m_u4;
     // примитивные переменные
     double* m_rho, * m_w1, * m_w2, * m_w3, * pressure;
     // универсальная газовая постоянная [Дж / (кг * К)]
     double m_R = 286.7;
+
+protected:
+
+    // пересчитывает консервативные переменные в примитивные
+    void Decoding();
+
+    // gmsh-файл в который сохраняется решение
+    std::string m_gmsh_file = "C:/Users/wchhi/source/repos/EulerProj/EulerProj/results/res.msh";
+    // ДЛЯ ЗАДАЧИ: время = 1, шаг = 0.1
+    double m_time_moment = 1;
+    uint32_t m_step = 0.1;
 };
 
 #endif // EULER_H
